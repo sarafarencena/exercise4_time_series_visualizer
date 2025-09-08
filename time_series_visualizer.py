fd9c2ed038d0743d514ed4cefc2777a34ff89927
Sara Sbardelotto <smfsapp@gmail.com>
2025-09-08 02:55:06 +0000

    feat: answer project questions

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,17 +5,28 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+# Usei parse_dates para converter a coluna date para datetime e index_col para definir date como índice 
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date'], index_col=['date'])
 
 # Clean data
-df = None
+# Na limpeza de dados, removi os outliers e mantive apenas valores entre os percentis de 2,5 e 97,5, porém, essa parte não passa no teste pois, de acordo com o que pesquisei, no test_module.py deveria ser usado df.count().sum() ou df['value'].count() para isso.
+df = df.loc[
+    (df['value'] >= df['value'].quantile(0.025)) &
+    (df['value'] <= df['value'].quantile(0.975))
+]
 
 
 def draw_line_plot():
     # Draw line plot
+    fig, ax = plt.subplots(figsize=(12,6))
 
-
-
+    # Para plotar os dados ao longo do tempo:
+    ax.plot(df.index, df['value'], 'red', linewidth=1)
+    
+    # Para definir o título do gráfico e a descrição dos eixos (x, y):
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
 
 
     # Save image and return fig (don't change this part)
@@ -24,13 +35,22 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.copy()
+
+    # Para extrair mês e ano, e agrupar depois:
+    df["month"] = df.index.month
+    df["year"] = df.index.year
+
+    # Agrupa os dados por ano e mês, depois calcula a média de page views por mês e transforma os meses em colunas
+    df_bar = df.groupby(["year", "month"])["value"].mean().unstack()
 
     # Draw bar plot
-
-
-
-
+    fig, ax = plt.subplots(figsize=(12, 6))
+    df_bar.plot.bar(ax=ax)
+    ax.set_xlabel("Years")
+    ax.set_ylabel("Average Page Views")
+    ax.legend(['January', 'February', 'March', 'April', 'May', 'June', 
+              'July', 'August', 'September', 'October', 'November', 'December'], title='Months')
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
@@ -44,9 +64,23 @@ def draw_box_plot():
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
+    # Adiciona coluna com número do mês para deixar os meses em ordem no gráfico
+    df_box["month_num"] = df_box["date"].dt.month
+    df_box = df_box.sort_values("month_num")
 
+    fig, axes= plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
 
+    # Gráficos de boxplot por ano mostra tendência) e por mês (mostra sazonalidade)
+    axes[0] = sns.boxplot(x=df_box["year"], y=df_box["value"], ax = axes[0])
+    axes[1] = sns.boxplot(x=df_box["month"], y=df_box["value"], ax = axes[1])
 
+    axes[0].set_title("Year-wise Box Plot (Trend)")
+    axes[0].set_xlabel("Year")
+    axes[0].set_ylabel("Page Views")
+
+    axes[1].set_title("Month-wise Box Plot (Seasonality)")
+    axes[1].set_xlabel("Month")
+    axes[1].set_ylabel("Page Views")
 
 
     # Save image and return fig (don't change this part)
